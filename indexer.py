a01df129fc25bdd792f1eec5366dad207ef6a991
Nicolas Gatien <nicolas.gatien6@gmail.com>
2026-01-18 22:33:16 -0500

    batch requests
    resolves #6

---
+++ indexer.py
@@ -0,0 +1,121 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import nltk
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+def get_soup(url: str) -> BeautifulSoup:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
+
+def get_keywords(content: str):
+    tokens = nltk.RegexpTokenizer(r'\w+').tokenize(content.lower())
+    tagged = nltk.pos_tag(tokens)
+
+    keywords = {}
+
+    irrelevent_tags = ["DT", ":", "IN", "TO", "PRP", "JJ"]
+
+    for i, (word, tag) in enumerate(tagged):
+        if tag in irrelevent_tags:
+            continue
+
+        if word in keywords:
+            keywords[word].append(i)
+        else:
+            keywords[word] = [i]
+
+    return keywords
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+
+while True:
+    queue = supabase.table('known_pages').select("*").execute().data
+    if len(queue) == 0:
+        break
+
+    url = urlparse(queue[0]['url'])
+    print(url.geturl())
+    hostname = url.hostname
+
+    soup = get_soup(url.geturl())
+    keywords = get_keywords(soup.get_text("\n"))
+
+    res = (
+        supabase.table("sites")
+           .insert({"url": url.geturl(), "doc_length": len(keywords)})
+           .execute()
+        )
+    site_id = res.data[0]['site_id']
+
+    posting_rows = []
+    words = list(keywords.keys())
+
+    existing_keywords = (
+        supabase.table('keywords')
+        .select("*")
+        .in_("keyword", words)
+        .execute()
+    ).data
+
+    existing_map = {
+        row["keyword"]: row
+        for row in existing_keywords
+    }
+
+    keyword_upserts = []
+    for word in words:
+        if word in existing_map:
+            keyword_upserts.append({
+                "keyword": word,
+                "document_frequency": existing_map[word]['document_frequency'] + 1
+            })
+        else:
+            keyword_upserts.append({
+                "keyword": word,
+                "document_frequency": 1
+            })
+
+
+    supabase.table('keywords').upsert(
+        keyword_upserts,
+        on_conflict="keyword"
+    ).execute()
+
+    keyword_rows = (
+        supabase.table('keywords')
+        .select('keyword_id', 'keyword')
+        .in_('keyword', words)
+        .execute()
+    ).data
+
+    keyword_id_map = {
+        row['keyword']: row['keyword_id']
+        for row in keyword_rows
+    }
+
+    posting_rows = []
+    for word, positions in keywords.items():
+        posting_rows.append({
+            "keyword_id": keyword_id_map[word],
+            "site_id": site_id,
+            "term_frequency": len(positions),
+            "positions": positions
+        })
+
+    response = (
+            supabase.table("postings")
+            .insert(posting_rows)
+            .execute()
+        )
+    
+    supabase.table('known_pages').delete().eq("url", url.geturl()).execute()
+    print(queue)
